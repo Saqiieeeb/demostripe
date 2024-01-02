3e23c76689cd4b80a99e91040254009d95687fd0
Saqiieeeb <saqibshaikh160@gmail.com>
2024-01-02 12:13:18 +0530

    changes

--- index.js
+++ index.js
@@ -82,8 +82,8 @@ app.post("/create-checkout-session", async (req, res) => {
     // ui_mode: "embeded",
     line_items: lineItems,
     mode: "payment",
-    success_url: `http://localhost:5173/success`,
-    cancel_url: `http://localhost:5173/cart`,
+    success_url: `https://demostripe.netlify.app/success`,
+    cancel_url: `https://demostripe.netlify.app//cart`,
   });
 
   res.json({ id: session.id });
